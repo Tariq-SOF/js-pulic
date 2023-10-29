ba101b29c93c0d99e1a4248a3bae03a91de6bc18
Tariq-SOF <tari.almutairi1995@gmai.com>
2023-10-29 15:29:28 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+let height= 19;
+let Width= 20;
+let rectangle;
+console.log(rectangle=height*Width);
+
+let x=32;
+let y= 1.8;
+let h=10;
+let F;
+F=y*10+x;
+console.log(F);
+h=F-x;
+console.log(h=F-x);
+
+let H=1;
+let S=60;
+let m;
+m=h*S*S;
+console.log(m);
+
+let z=80;
+if(z <= 100 && z >=95){
+    console.log("A+");
+}else if(z<= 94 && z >= 90){
+    console.log("A");
+}else if(z<=89 && z >=85){
+    console.log("B+");
+}else if(z <=84 && z >=80){
+    console.log("B");
+}else if(z <=79 && z >=75){
+    console.log("C+");
+}else if(z<=74 && z>=60){
+    console.log("C");
+}else if(z<=59 && z>=55){
+    console.log("D+");
+}else if(z<=54 && z>=50){
+    console.log("D");
+}else if(z<=49 && z>=0){
+    console.log("F");
+}
